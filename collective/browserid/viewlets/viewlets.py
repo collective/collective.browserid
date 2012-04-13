0b4929f903ce3019fc8c3e1a57e062021d9f0807
Richard Mitchell <richard.j.mitchell@gmail.com>
2012-04-13 10:17:13 +0100

    Move condition for rendering of browserid JS to viewlet code.

--- collective/browserid/viewlets/viewlets.py
+++ collective/browserid/viewlets/viewlets.py
@@ -4,4 +4,7 @@ from plone.app.layout.viewlets.common import ViewletBase
 class JavascriptViewlet(ViewletBase):
     
     def index(self):
-        return '<script type="text/javascript" src="https://browserid.org/include.js"></script>'
+        if getattr(self.context, 'hasBrowserIdExtractor', lambda: False)():
+            return '<script type="text/javascript" src="https://browserid.org/include.js"></script>'
+        
+        return ''
